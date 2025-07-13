77bbf0dfafa4db958e59fc1b31143b5f4e67056b
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-07-13 22:00:26 +0200

    update CT client to accept Post requests, too

--- Classes/ChurchTools.ps1
+++ Classes/ChurchTools.ps1
@@ -5,10 +5,41 @@ class ChurchTools {
     [string]$CachePath
 
     ChurchTools([string]$baseUrl, [string]$token) {
-        $this.BaseUrl = $baseUrl.TrimEnd("/")
+        $this.BaseUrl = $baseUrl
         $this.Headers = @{ Authorization = "Login $($token)" }
         $this.CachePath = "$PSScriptRoot\..\.usercache.json"
         $this.LoadUserData()
+    }
+
+    [object] CallApi([string]$Method, [string]$Path, [object]$Body, [string]$OutFile) {
+        if ($Path -match '^https?://') {
+            $uri = $Path
+        } else {
+            $uri = "$($this.BaseUrl)/$Path"
+        }
+
+        $params = @{
+            Method      = $Method
+            Uri         = $uri
+            Headers     = $this.Headers
+            ErrorAction = 'Stop'
+        }
+
+        if ($Body -ne $null) {
+            $jsonBody = $Body | ConvertTo-Json -Depth 10
+            $params['Body'] = $jsonBody
+            $params['ContentType'] = 'application/json'
+        }
+
+        if ($OutFile) {
+            $params['OutFile'] = $OutFile
+            Invoke-WebRequest @params
+            return $OutFile
+        } else {
+            $response = Invoke-WebRequest @params
+            $json = $response.Content | ConvertFrom-Json
+            return $json.data
+        }
     }
 
     [void] LoadUserData() {
@@ -16,8 +47,8 @@ class ChurchTools {
             $this.User = Get-Content $this.CachePath -Raw | ConvertFrom-Json
         } else {
             try {
-                $userData = $this.CallApi("GET", "whoami", $null)
-                $groups = $this.CallApi("GET", "persons/$($userData.id)/groups", $null)
+                $userData = $this.CallApi("GET", "whoami", $null, $null)
+                $groups = $this.CallApi("GET", "persons/$($userData.id)/groups", $null, $null)
                 $this.User = [PSCustomObject]@{
                     firstName   = $userData.firstName
                     lastName = $userData.lastName
@@ -34,22 +65,5 @@ class ChurchTools {
 
     [void] CacheUserData() {
         $this.User | ConvertTo-Json | Set-Content -Path $this.CachePath
-    }
-
-    [object] CallApi([string]$Method, [string]$Path, [string]$OutFile) {
-        if ($Path -match '^https?://') {
-            $uri = $Path
-        } else {
-            $uri = "$($this.BaseUrl)/$Path"
-        }
-
-        if ($OutFile) {
-            Invoke-WebRequest -Method $Method -Uri $uri -Headers $this.Headers -OutFile $OutFile -ErrorAction Stop
-            return $Outfile
-        } else {
-            $response = Invoke-WebRequest -Method $Method -Uri $uri -Headers $this.Headers -ErrorAction Stop
-            $json = $response.Content | ConvertFrom-Json
-            return $json.data
-        }
     }
 }
