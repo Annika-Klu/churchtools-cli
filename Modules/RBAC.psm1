e444f9e8fdc02d02b195f889c716b07b11aad8f0
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-07-13 23:24:25 +0200

    RBAC module: get allowed commands based on user groups

---
+++ Modules/RBAC.psm1
@@ -0,0 +1,30 @@
+function Get-AllowedCommands {
+    $allowedCommands = @()
+    $ct = [ChurchTools]::new($CT_API_URL, $CT_API_TOKEN)
+
+    Get-ChildItem -Path "$PSScriptRoot\..\Commands" -Filter *.ps1 -Recurse | ForEach-Object {
+        $commandFile = $_.FullName
+        $accessFile = Join-Path $_.Directory.FullName ".access"
+
+        $isAllowed = $false
+
+        if (Test-Path $accessFile) {
+            $content = Get-Content $accessFile -Raw
+            if ($content -match '^groups\s*=\s*(.+)$') {
+                $groupsString = $matches[1].Trim()
+                if ($groupsString -eq "*") {
+                    $isAllowed = $true
+                } else {
+                    $allowedGroups = $groupsString -split '\s*,\s*'
+                    $isAllowed = $ct.UserHasAccess($allowedGroups)
+                }
+            }
+        }
+
+        if ($isAllowed) {
+            $allowedCommands += $_
+        }
+    }
+
+    return $allowedCommands
+}
