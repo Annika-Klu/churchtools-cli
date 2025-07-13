250734f827e994a7ac73f3c3d752838e329e0543
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-07-13 22:02:07 +0200

    rm old Show-Help function

--- Commands/hilfe.ps1
+++ Commands/hilfe.ps1
@@ -1,20 +1,3 @@
-function Show-Help-Old {
-    Write-Host "Verfügbare Befehle:`n"
-    Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 | ForEach-Object {
-        $name = $_.BaseName
-        $descFile = $_.FullName -replace "\.ps1$", ".md"
-
-        if (Test-Path $descFile) {
-            $desc = Get-Content $descFile -Raw
-            $firstLine = $desc -split "`n" | Select-Object -First 1
-        } else {
-            $firstLine = "(Keine Beschreibung)"
-        }
-
-        Write-Host ("- {0}`t{1}" -f $name, $firstLine)
-    }
-}
-
 function Show-Help {
     Write-Host "Verfügbare Befehle:`n"
 
