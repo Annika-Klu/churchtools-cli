5094c2bf86ce377b6cdb2988fd34acc3b0dec287
Klüpfel Annika <AnnikaKluepfel@web.de>
2025-07-20 22:36:16 +0200

    GitHub module for getting latest release or finding asset

---
+++ cli_code/Modules/GitHub.psm1
@@ -0,0 +1,31 @@
+function Get-LatestRelease {
+    param(
+        [string]$ReleasesUrl
+    )
+    $response = Invoke-WebRequest -Uri $ReleasesUrl
+    $releases = $response.Content | ConvertFrom-Json
+    if ($releases.Count -eq 0) {
+        throw "Kein Release gefunden."
+    }
+    $latestRelease = $releases | Sort-Object { [datetime]$_.published_at } -Descending | Select-Object -First 1
+    return $latestRelease
+}
+
+function Get-ReleaseAsset {
+    param(
+        [PSObject]$Release,
+        [string]$AssetName
+    )
+    $assetsResponse = Invoke-WebRequest -Uri $Release.assets_url
+    $assets = $assetsResponse.Content | ConvertFrom-Json
+    if ($assets.Count -eq 0) {
+        throw "Keine Assets für Release $($Release.tag_name) gefunden."
+    }
+    $asset = $assets | Where-Object { $_.name -eq $AssetName }
+    if (-not $asset) {
+        throw "Die relevanten Dateien wurden nicht in den Release Assets gefunden."
+    }
+    return $asset
+}
+
+Export-ModuleMember -Function Get-LatestRelease, Get-ReleaseAsset
